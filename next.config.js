ea7a5daab68bf1d604c0d82f3d59035354ea906c
Agustín Lozano <agustinlozano@example.com>
2026-02-16 15:49:59 -0600

    fix: clean up next.config.js - remove invalid experimental config

--- next.config.js
+++ next.config.js
@@ -3,14 +3,6 @@ const nextConfig = {
   images: {
     domains: ['firebasestorage.googleapis.com'],
   },
-  experimental: {
-    isrMemoryCacheSize: 0,
-  },
-  // Ignore static generation errors
-  onDemandEntries: {
-    maxInactiveAge: 25 * 1000 * 60,
-    pagesBufferLength: 5,
-  },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
